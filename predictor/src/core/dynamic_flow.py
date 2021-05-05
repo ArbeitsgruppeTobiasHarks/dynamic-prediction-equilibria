174e22b8011da64859d8a525648c201176071eed
Michael Markl <marklmichael98@gmail.com>
2021-05-06 01:10:51 +0200

    Use dynamic time steps

--- predictor/src/core/dynamic_flow.py
+++ predictor/src/core/dynamic_flow.py
@@ -1,4 +1,5 @@
-from typing import List, Optional, Dict
+from heapq import heappush, heappop
+from typing import List, Dict
 
 import numpy as np
 
@@ -7,45 +8,59 @@ from core.network import Network
 
 class PartialDynamicFlow:
     """
-        This is a representation of a flow with constant edge inflow rates on discrete time steps of equal length.
-        We assume, that all edge travel times are a multiple of the time_step
+        This is a representation of a flow with right-constant edge inflow rates on intervals.
     """
 
     # We use List for fast extensions
-    inflow: List[np.ndarray]  # inflow[t][e] is the constant inflow in e at times [t, t + time_step_size)
-    queues: List[np.ndarray]  # queue[t][e] is the queue length at e at time t
+    times: List[float]  #
+    inflow: List[np.ndarray]  # inflow[k][e] is the constant value of fₑ⁺ on [times[k], times[k+1])
+    queues: List[np.ndarray]  # queue[k][e] is the queue length at e at time times[k]
+    change_events: List[float]  # A heapq priority queue with times where some edge outflow changes
 
-    __time_step_size: int
     __network: Network
 
-    def __init__(self, time_step_size: int, network: Network):
+    def __init__(self, network: Network):
         self.__network = network
-        self.__time_step_size = time_step_size
-        assert all(tau % time_step_size == 0 for tau in network.travel_time)
+        self.times = [0.]
         self.inflow = []
         self.queues = [np.zeros(len(network.graph.edges))]
+        self.change_events = []
 
-    def extend(self, new_inflow: np.ndarray, verify_balance: Optional[Dict[int, float]]):
+    def extend(self, new_inflow: np.ndarray, max_extension_length: float) -> float:
         """
-        Extends the flow with constant inflows new_inflow until the next time step.
-        If verify_balance is given, it checks if the extension satisfies the balances given
+        Extends the flow with constant inflows new_inflow until some edge outflow changes.
+        The user can also specify a maximum extension length using max_extension_length.
+        If verify_balance is given, it checks if the extension satisfies the balances given.
+        :returns The length of the actual extension
         """
+        phi = self.times[-1]
+        m = len(self.__network.graph.edges)
+        capacity = self.__network.capacity
+        travel_time = self.__network.travel_time
+
+        # determine how long we can extend without changing any edge outflow
+        for e in range(m):
+            if self.queues[-1][e] > 0:
+                if new_inflow[e] < capacity[e]:
+                    heappush(
+                        self.change_events,
+                        phi + travel_time[e] + self.queues[-1][e] / (capacity[e] - new_inflow[e])
+                    )
+            elif self.queues[-1][e] == 0 and new_inflow[e] != (0 if phi == 0 else self.inflow[-1][e]):
+                heappush(self.change_events, phi + travel_time[e])
+
+        first_event = min(self.change_events, default=float('inf'))
+        eps = min(first_event - phi, max_extension_length)
+        if eps < max_extension_length:
+            while min(self.change_events, default=float('inf')) <= first_event:
+                heappop(self.change_events)
 
         self.inflow.append(new_inflow)
         # qₑ(θ + ε) = max{ 0, qₑ(θ) + ε(fₑ⁺ - νₑ) }
-        self.queues.append(np.maximum(
-            0,
-            self.queues[-1] + self.__time_step_size * (new_inflow - self.__network.capacity)
-        ))
+        self.queues.append(np.maximum(0, self.queues[-1] + eps * (new_inflow - capacity)))
+        self.times.append(phi + eps)
 
-        if verify_balance is not None:
-            current_outflow = self.current_outflow()
-            for (node_id, balance) in verify_balance:
-                assert node_id in self.__network.graph.nodes.keys(), f"Could not find node#{node_id}"
-                node = self.__network.graph.nodes[node_id]
-                node_inflow = sum(current_outflow[e.id] for e in node.incoming_edges)
-                node_outflow = sum(new_inflow[e.id] for e in node.outgoing_edges)
-                assert balance == node_inflow - node_outflow, f"Balance for node#{node_id} does not match"
+        return phi + eps
 
     def current_outflow(self) -> np.ndarray:
         t = len(self.inflow)
@@ -60,3 +75,12 @@ class PartialDynamicFlow:
         ])
 
         return np.where(queue_on_entry, self.__network.capacity, inflow_on_entry)
+
+    def verify_balance(self, new_inflow: np.ndarray, verify_balance: Dict[int, float]):
+        current_outflow = self.current_outflow()
+        for (node_id, balance) in verify_balance:
+            assert node_id in self.__network.graph.nodes.keys(), f"Could not find node#{node_id}"
+            node = self.__network.graph.nodes[node_id]
+            node_inflow = sum(current_outflow[e.id] for e in node.incoming_edges)
+            node_outflow = sum(new_inflow[e.id] for e in node.outgoing_edges)
+            assert balance == node_inflow - node_outflow, f"Balance for node#{node_id} does not match"
--- predictor/src/test/sample_flow.py
+++ predictor/src/test/sample_flow.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from core.dynamic_flow import PartialDynamicFlow
+from heapq import nsmallest
 from test.sample_network import build_sample_network
 
 
@@ -13,18 +14,19 @@ class TestPartialDynamicFlow(unittest.TestCase):
         """
         network = build_sample_network()
         m = len(network.graph.edges)
-        time_step_size = 1
-        flow = PartialDynamicFlow(time_step_size, network)
+        flow = PartialDynamicFlow(network)
 
         self.assertTrue(np.array_equal(flow.queues[0], np.zeros(m)), "Queues don't match at time 0!")
 
-        flow.extend(np.array([2, 1, 0, 0, 0]), None)
-        self.assertTrue(np.array_equal(flow.queues[1], np.zeros(m)), "Queues don't match at time 1!")
+        time1 = flow.extend(np.array([2, 1, 0, 0, 0]), float('inf'), None)
+        self.assertEqual(time1, 1.)
+        self.assertTrue(np.array_equal(flow.queues[1], np.zeros(m)), f"Queues don't match at time {time1}!")
+        self.assertEqual(nsmallest(2, flow.change_events), [1., 3.])
 
-        flow.extend(np.array([0, 0, 2, 0, 4]), None)
+        flow.extend(np.array([0, 0, 2, 0, 4]), float('inf'), None)
         self.assertTrue(np.array_equal(flow.queues[2], np.array([0, 0, 0, 0, 3])))
 
-        flow.extend(np.array([0, 0, 0, 1, 1]), None)
+        flow.extend(np.array([0, 0, 0, 1, 1]), float('inf'), None)
         self.assertTrue(np.array_equal(flow.queues[3], np.array([0, 0, 0, 0, 3])))
 
 
