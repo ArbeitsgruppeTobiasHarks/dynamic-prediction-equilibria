Fix errors with machine precision eps

predictor/src/core/machine_precision.py
@@ -1,3 +1,1 @@
-import numpy as np
-
-eps = np.finfo(float).eps * 10
+eps = 1e-10