de7bc0dd2dddfd934da81e1d40f4281ba713f8ad
Michael Markl <marklmichael98@gmail.com>
2021-05-18 20:31:27 +0200

    Add function for adding commodities from csv and fix import of capacity and travel time

--- predictor/src/importer/csv_importer.py
+++ predictor/src/importer/csv_importer.py
@@ -1,6 +1,9 @@
 from numpy import genfromtxt
 
+from core.constant_predictor import ConstantPredictor
+from core.multi_com_flow_builder import MultiComFlowBuilder
 from core.network import Network
+from core.single_edge_distributor import SingleEdgeDistributor
 
 
 def network_from_csv(path: str) -> Network:
@@ -11,10 +14,25 @@ def network_from_csv(path: str) -> Network:
     # We default to 20 for now.
 
     network = Network()
-    for row in np_data:
+    for i, row in enumerate(np_data):
         freeflow_speed = row[3] if row[3] > 0 else 20.
-        travel_time = row[2] * freeflow_speed
-        capacity = row[3] * freeflow_speed
+        travel_time = row[2] / freeflow_speed
+        capacity = row[4] * freeflow_speed
 
+        if row[0] == row[1]:
+            print("Edge #{i} has not been added, as it's a loop from and to node #{row[0]}!")
+            continue
         network.add_edge(row[0], row[1], travel_time, capacity)
     return network
+
+
+def add_demands_to_network(network: Network, demands_path: str):
+    np_data = genfromtxt(demands_path, delimiter=' ')
+    for i, row in enumerate(np_data):
+        # Filter unrealistic / impractical commodities where the source cant reach the sink
+        source = network.graph.nodes[row[0]]
+        sink = network.graph.nodes[row[1]]
+        if source not in network.graph.get_nodes_reaching(sink):
+            print(f"Did not add the commodity of row {i}! The source #{source} can not reach the sink #{sink}!")
+        else:
+            network.add_commodity(row[0], row[1], row[2])
