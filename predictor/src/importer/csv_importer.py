8639edf9b9e2ef6b2f1505017d38c58e3beadc33
Michael Markl <marklmichael98@gmail.com>
2021-05-19 12:08:28 +0200

    Add csv_importer main

--- predictor/src/importer/csv_importer.py
+++ predictor/src/importer/csv_importer.py
@@ -36,3 +36,35 @@ def add_demands_to_network(network: Network, demands_path: str):
             print(f"Did not add the commodity of row {i}! The source #{source} can not reach the sink #{sink}!")
         else:
             network.add_commodity(row[0], row[1], row[2])
+
+
+if __name__ == '__main__':
+    network_path = '/home/michael/Nextcloud2/Universität/2021-SS/softwareproject/data/from-kostas/tokyo_small.arcs'
+    network = network_from_csv(network_path)
+    demands_path = '/home/michael/Nextcloud2/Universität/2021-SS/softwareproject/data/from-kostas/tokyo.demands'
+    add_demands_to_network(network, demands_path)
+
+    max_in_degree = 0
+    max_out_degree = 0
+    for node in network.graph.nodes.values():
+        max_in_degree = max(max_in_degree, len(node.incoming_edges))
+        max_out_degree = max(max_out_degree, len(node.outgoing_edges))
+    print(f"Maximum indgree: {max_in_degree}")
+    print(f"Maximum outdegree: {max_out_degree}")
+
+    predictor = ConstantPredictor(network)
+    distributor = SingleEdgeDistributor(network)
+    max_extension_length = 1
+    horizon = 20
+
+    flow_builder = MultiComFlowBuilder(network, predictor, distributor, reroute_interval=0.1)
+    generator = flow_builder.build_flow()
+    flow = next(generator)
+    phi = flow.times[-1]
+    while phi < horizon:
+        flow = next(generator)
+        phi = flow.times[-1]
+        print(f"phi={phi}")
+
+    flow.to_file("./")
+    print(f"Successfully built flow up to time {phi}!")
