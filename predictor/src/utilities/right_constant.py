7e7af978d00da74837db16f0c387fe80049f0ade
Michael Markl <marklmichael98@gmail.com>
2021-05-25 14:57:57 +0200

    Add sum and integral to RightConstantFunction

--- predictor/src/utilities/right_constant.py
+++ predictor/src/utilities/right_constant.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from typing import List, Tuple
 
 from core.machine_precision import eps
-from utilities.arrays import elem_lrank
+from utilities.arrays import elem_lrank, merge_sorted
+from utilities.interpolate import LinearlyInterpolatedFunction
 
 
 class RightConstantFunction:
@@ -58,3 +59,20 @@ class RightConstantFunction:
         if not isinstance(other, RightConstantFunction):
             return False
         return self.values == other.values and self.times == other.times and self.domain == other.domain
+
+    def __radd__(self, other):
+        if not isinstance(other, RightConstantFunction):
+            raise TypeError("Can only add a RightConstantFunction.")
+        assert self.domain == other.domain
+
+        new_times = merge_sorted(self.times, other.times)
+        new_values = [self(t) + other(t) for t in new_times]
+        return RightConstantFunction(new_times, new_values, self.domain)
+
+    def integral(self) -> LinearlyInterpolatedFunction:
+        assert self.times[0] == self.domain[0] and self.domain[1] >= self.times[-1] + 1
+        times = self.times + [self.times[-1] + 1]
+        values = [0.] * len(times)
+        for i in range(len(times) - 1):
+            values[i + 1] = values[i] + self.values[i] * (times[i + 1] - times[i])
+        return LinearlyInterpolatedFunction(times, values, self.domain)
