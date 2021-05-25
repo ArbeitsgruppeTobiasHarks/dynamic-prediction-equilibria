c74ac2c9780de89ac7a9ba860443855cd9a22d35
Michael Markl <marklmichael98@gmail.com>
2021-05-25 17:42:50 +0200

    Try a bunch of stuff without success

--- predictor/src/gnn/DataLoader.py
+++ predictor/src/gnn/DataLoader.py
@@ -14,20 +14,35 @@ from core.network import Network
 class QueueDataset(Dataset):
     _queues: List[Tuple[Tensor, Tensor]]
     _flow: Optional[MultiComPartialDynamicFlow]
+    test_mask: torch.Tensor
 
     def __init__(self, folder_path: str, past_timesteps: int, future_timesteps: int, network: Network):
         queue_dirs = [os.path.join(folder_path, file) for file in os.listdir(folder_path)]
         self._queues = []
         assert len(queue_dirs) > 0
         print("Reading in dataset...")
+        min_queue_val = float('inf')
+        max_queues = torch.zeros(len(network.graph.edges))
         for directory in queue_dirs:
             past_queues = torch.load(os.path.join(directory, "past_queues.pt"))
-            future_queues = torch.load(os.path.join(directory, "future_queues.pt"))
+            future_queues = torch.load(os.path.join(directory, "future_queues.pt"))[:, 0:future_timesteps]
+            max_queues = torch.maximum(max_queues,
+                                       torch.maximum(
+                                           torch.amax(past_queues, dim=1),
+                                           torch.amax(future_queues, dim=1)
+                                       ))
+            min_queue_val = min(min_queue_val, torch.min(past_queues), torch.min(future_queues))
             self._queues.append((past_queues, future_queues))
+
+        self.test_mask = torch.tensor([max_queue > 0 for max_queue in max_queues]).to('cuda')
+
+        print(f"min_queue_val={min_queue_val}")
         print("Done reading dataset.")
 
     def __getitem__(self, index) -> T_co:
-        return self._queues[index][0].to('cuda'), self._queues[index][1].to('cuda')
+        input_sample = self._queues[index][0].to('cuda')[self.test_mask]
+        label_sample = self._queues[index][1].to('cuda')[self.test_mask]
+        return input_sample, label_sample
 
     def __len__(self):
         return len(self._queues)
@@ -49,10 +64,10 @@ def generate_queues(past_timesteps: int, future_timesteps: int):
             random.seed(i)
             pred_time = float(random.randint(0, 300))
             times_past_queues = [
-                pred_time - i * i * step_size for i in range(past_timesteps)
+                pred_time - i * step_size for i in range(past_timesteps)
             ]
             times_future_queues = [
-                pred_time + (i + 1) * (i + 1) * step_size for i in range(future_timesteps)
+                pred_time + i * step_size for i in range(1, future_timesteps + 1)
             ]
             past_queues = torch.tensor([
                 [queue(time) for time in times_past_queues] for queue in flow.queues
--- predictor/src/gnn/Model.py
+++ predictor/src/gnn/Model.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 import dgl
+import numpy as np
 import torch
 import torch.nn as nn
-from dgl.nn.pytorch import GraphConv
+from dgl.nn.pytorch import GraphConv, GATConv
 from torch.utils.data import DataLoader
 
 from gnn.DataLoader import QueueDataset
@@ -13,19 +16,34 @@ class Model(nn.Module):
         super(Model, self).__init__()
         assert conv_layers >= 2
         first_layer_dim = 2 + past_queue_steps
-        middle_layer_dim = first_layer_dim * future_queue_steps * 50
+        middle_layer_dim = 1000
         self.layers = nn.ModuleList([
-            GraphConv(first_layer_dim, middle_layer_dim, activation=torch.relu)
+            GraphConv(first_layer_dim, middle_layer_dim, norm='none', activation=torch.relu)
         ])
         for i in range(1, conv_layers - 1):
-            self.layers.append(GraphConv(middle_layer_dim, middle_layer_dim, activation=torch.relu))
-        self.layers.append(GraphConv(middle_layer_dim, future_queue_steps, activation=torch.relu))
+            self.layers.append(GraphConv(middle_layer_dim, middle_layer_dim, norm='none', activation=torch.relu))
+        self.layers.append(GraphConv(middle_layer_dim, future_queue_steps, norm='none'))
 
     def forward(self, g, input):
         x = input
         for i, layer in enumerate(self.layers):
-            x = layer(g if i % 2 == 0 else g.reverse(), x)
+            x = layer(g.reverse() if i % 4 == 3 else g, x)
         return x
+
+
+class DenseLayerModel(nn.Module):
+    def __init__(self, in_features: int, num_edges: int, out_features: int):
+        super(DenseLayerModel, self).__init__()
+        hidden_features = 10
+        self.first = nn.Linear(in_features * num_edges, hidden_features * num_edges)
+        self.second = nn.Linear(hidden_features * num_edges, hidden_features * num_edges)
+        self.last = nn.Linear(hidden_features * num_edges, out_features * num_edges)
+
+    def forward(self, input):
+        return self.last(
+            torch.relu(self.second(
+                torch.relu(self.first(input))
+            )))
 
 
 if __name__ == '__main__':
@@ -37,19 +55,28 @@ if __name__ == '__main__':
     v = torch.tensor([e[1] for e in new_edges]).to('cuda')
     graph = dgl.graph((u, v)).add_self_loop().to('cuda')
 
-    past_timesteps, future_timesteps = 5, 5
+    past_timesteps, future_timesteps = 5, 1
+    conv_layers = 6
     queue_folder_path = '/home/michael/Nextcloud/Universität/2021-SS/softwareproject/data/generated_queues/5,5/'
     queue_dataset = QueueDataset(queue_folder_path, past_timesteps, future_timesteps, network)
 
-    capacity = torch.from_numpy(network.capacity).float().to('cuda')
-    travel_time = torch.from_numpy(network.travel_time).float().to('cuda')
+    mask = queue_dataset.test_mask.cpu().numpy()
+    np.savetxt('/home/michael/Nextcloud/Universität/2021-SS/softwareproject/data/generated_queues/5,5/mask.txt', mask,
+               fmt="%d")
 
-    conv_layers = 20
-    model = Model(past_timesteps, future_timesteps, conv_layers).to('cuda')
+    # subgraph_nodes = torch.nonzero(queue_dataset.test_mask, as_tuple=True)[0]
+    # graph = dgl.node_subgraph(graph, subgraph_nodes)
+    num_edges = torch.count_nonzero(queue_dataset.test_mask).item()
+
+    # model = Model(past_timesteps, future_timesteps, conv_layers).to('cuda')
+    model = DenseLayerModel(past_timesteps + 2, num_edges, future_timesteps) \
+        .to('cuda')
+
+    capacity = torch.from_numpy(network.capacity).float().to('cuda')[queue_dataset.test_mask]
+    travel_time = torch.from_numpy(network.travel_time).float().to('cuda')[queue_dataset.test_mask]
 
 
     def collate(samples):
-        torch.stack([sample[0] for sample in samples])
         input = torch.stack([torch.column_stack([capacity, travel_time, sample[0]]) for sample in samples], dim=1)
         label = torch.stack([sample[1] for sample in samples], dim=1)
         return input, label
@@ -58,27 +85,33 @@ if __name__ == '__main__':
     data_loader = DataLoader(queue_dataset, batch_size=1, shuffle=True, collate_fn=collate)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    loss_fct = nn.L1Loss()
+    loss_fct = nn.MSELoss()
+    model.train()
 
     epoch_losses = []
     for epoch in range(80):
         epoch_loss = 0
         k = 0
         for k, (input, label) in enumerate(data_loader):
-            prediction = model(graph, input)
-            loss = loss_fct(prediction, label)
             optimizer.zero_grad()
+            prediction = model(torch.flatten(input))
+            reshaped = torch.reshape(prediction, (num_edges, future_timesteps))
+            loss = loss_fct(reshaped, label)
             loss.backward()
             optimizer.step()
             epoch_loss += loss.detach().item()
-            print(f".. batch {k}, loss {loss:.4f}, rolling avg: {epoch_loss / (k+1):.4f}")
+            max_difference = torch.max(torch.abs(label - reshaped))
+
+            print(
+                f".. batch {k}, loss {loss:.4f}, max_diff {max_difference:.4f}, rolling avg: {epoch_loss / (k + 1):.4f}")
 
         epoch_loss /= (k + 1)
         print('Epoch {}, loss {:.4f}'.format(epoch, epoch_loss))
+        time = datetime.now().strftime("%H:%M:%S")
         torch.save({
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
             'loss': epoch_loss
-        }, f"./checkpoint.{epoch}.{epoch_loss:.2f}")
+        }, f"../../out/{time}-{epoch}-{epoch_loss:.2f}.chk")
         epoch_losses.append(epoch_loss)
