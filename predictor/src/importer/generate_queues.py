2c82381cce57bd4af84f6b5fbe577aaab684576c
Michael Markl <marklmichael98@gmail.com>
2021-06-10 12:54:00 +0200

    Generate all queues in a single file

--- predictor/src/importer/generate_queues.py
+++ predictor/src/importer/generate_queues.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def generate_queues(past_timesteps: int, future_timesteps: int, flows_folder: str, out_folder: str):
     os.makedirs(out_folder, exist_ok=True)
-    samples_per_flow = 200
+    horizon = 100
     step_size = 1.
 
     files = [file for file in os.listdir(flows_folder) if file.endswith(".flow.pickle")]
@@ -16,18 +16,8 @@ def generate_queues(past_timesteps: int, future_timesteps: int, flows_folder: st
         with open(os.path.join(flows_folder, flow_path), "rb") as file:
             flow = pickle.load(file)
 
-        for i in range(samples_per_flow):
-            random.seed(i)
-            pred_time = float(random.randint(0, 100))
-
-            times_past_queues = [
-                pred_time - i * step_size for i in range(past_timesteps)
-            ]
-            times_future_queues = [
-                pred_time + i * step_size for i in range(1, future_timesteps + 1)
-            ]
-            times = times_past_queues + times_future_queues
-            queues = np.asarray([
-                [queue(time) for time in times] for queue in flow.queues
-            ])
-            np.savetxt(os.path.join(out_folder, f"{flow_path}.{i}.csv"), queues)
+        times = range(-past_timesteps, horizon + future_timesteps)
+        queues = np.asarray([
+            [queue(time) for time in times] for queue in flow.queues
+        ])
+        np.savetxt(os.path.join(out_folder, f"{flow_path}.csv.gz"), queues)
