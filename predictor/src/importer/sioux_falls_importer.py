62ace8757770b17ed5b25401d368863a47f24931
Michael Markl <marklmichael98@gmail.com>
2021-09-03 15:32:19 +0200

    Add inflow horizon to sioux falls importer

--- predictor/src/importer/sioux_falls_importer.py
+++ predictor/src/importer/sioux_falls_importer.py
@@ -1,3 +1,5 @@
+import os
+import pathlib
 import random
 
 import pandas as pd
@@ -7,7 +9,7 @@ from core.predictors.predictor_type import PredictorType
 from utilities.right_constant import RightConstant
 
 
-def _generate_commodities(network: Network, number: int):
+def _generate_commodities(network: Network, number: int, inflow_horizon: float):
     assert number < len(network.graph.nodes)
     commodities = []
     nodes = list(network.graph.nodes.values())
@@ -17,13 +19,15 @@ def _generate_commodities(network: Network, number: int):
         if sink not in network.graph.get_reachable_nodes(source):
             continue
         demand = random.randint(4500, 10000)
-        network.commodities.append(
-            Commodity(source, sink, RightConstant([0.], [demand], (0, float('inf'))), PredictorType.CONSTANT)
-        )
+        if inflow_horizon < float('inf'):
+            commodity = Commodity(source, sink, RightConstant([0., inflow_horizon], [demand, 0.], (0, float('inf'))), PredictorType.CONSTANT)
+        else:
+            commodity = Commodity(source, sink, RightConstant([0.], [demand], (0, float('inf'))), PredictorType.CONSTANT)
+        network.commodities.append(commodity)
     return commodities
 
 
-def import_sioux_falls(file_path: str, out_file_path: str):
+def import_sioux_falls(file_path: str, out_file_path: str, inflow_horizon: float):
     net = pd.read_csv(file_path, skiprows=8, sep='\t')
     trimmed = [s.strip().lower() for s in net.columns]
     net.columns = trimmed
@@ -32,14 +36,16 @@ def import_sioux_falls(file_path: str, out_file_path: str):
     #  columns: init_node, term_node, capacity, length, free_flow_time, b, power, speed, toll, link_type
     for _, e in net.iterrows():
         network.add_edge(e["init_node"], e["term_node"], e["free_flow_time"], e["capacity"])
-    _generate_commodities(network, len(network.graph.nodes) // 4)
+    _generate_commodities(network, len(network.graph.nodes) // 2, inflow_horizon)
     network.remove_unnecessary_nodes()
     network.print_info()
+    os.makedirs(pathlib.Path(out_file_path).parent, exist_ok=True)
     network.to_file(out_file_path)
 
 
 if __name__ == '__main__':
     import_sioux_falls(
         "/home/michael/Nextcloud/Universität/2021/softwareproject/data/sioux-falls/SiouxFalls_net.tntp",
-        "/home/michael/Nextcloud/Universität/2021/softwareproject/data/sioux-falls/random-demands.pickle"
+        "/home/michael/Nextcloud/Universität/2021/softwareproject/data/sioux-falls/random-demands.pickle",
+        float('inf')
     )
