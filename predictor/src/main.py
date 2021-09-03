ea4839f74f8dec672cb8de6780fc1dbcbe6ef224
Michael Markl <marklmichael98@gmail.com>
2021-09-03 15:40:45 +0200

    Adjust main file

--- predictor/src/main.py
+++ predictor/src/main.py
@@ -2,10 +2,10 @@ import os
 
 import click
 
-import eval.evaluate_sample
 import eval.evaluate_network
-from importer.build_test_flows import build_flows_from_demand
-from importer.generate_queues import generate_queues
+import eval.evaluate_sample
+from ml.build_test_flows import build_flows
+from ml.generate_queues import generate_queues
 
 
 @click.group()
@@ -20,13 +20,13 @@ def main():
 def generate_flows(network_path: str, commodities_path: str, output_folder: str):
     print("Generating 200 Flows with random inflow between 20 and 100.")
     print("This might use up storage of ~22 GB")
-    build_flows_from_demand(network_path, commodities_path, os.path.join(output_folder, "flows"), 200)
+    build_flows(network_path, os.path.join(output_folder, "flows"), 200, 100, 2.5)
 
 
 @click.command(name='take_samples', help="Generate training data from generated flows.")
 @click.argument("output_folder", type=click.Path(exists=True, file_okay=False))
 def take_samples(output_folder: str):
-    generate_queues(5, 5, os.path.join(output_folder, "flows"), os.path.join(output_folder, "queues"))
+    generate_queues(5, os.path.join(output_folder, "flows"), os.path.join(output_folder, "queues"), 1, 1)
 
 
 @click.command(name='evaluate_sample', help="Evaluate the sample graph using demands in (0, 30)")
@@ -36,17 +36,15 @@ def evaluate_sample():
 
 @click.command(name='evaluate_network', help="Evaluate a network from file.")
 @click.argument("network_path", type=click.Path(exists=True, dir_okay=False))
-@click.argument("commodities_path", type=click.Path(exists=True, dir_okay=False))
 @click.argument("output_folder", type=click.Path(exists=True, file_okay=False))
-def evaluate_network(network_path: str, commodities_path: str, output_folder: str):
-    eval.evaluate_network.eval_network(network_path, commodities_path, output_folder)
+def evaluate_network(network_path: str, output_folder: str):
+    eval.evaluate_network.eval_network(network_path, output_folder)
 
 
 main.add_command(generate_flows)
 main.add_command(take_samples)
 main.add_command(evaluate_sample)
 main.add_command(evaluate_network)
-
 
 if __name__ == '__main__':
     main()
