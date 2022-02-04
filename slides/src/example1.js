efe56bcadca6c5444e771c0c04342baa69076687
Michael Markl <marklmichael98@gmail.com>
2022-02-04 15:19:21 +0100

    Enhance poster

--- slides/src/example1.js
+++ slides/src/example1.js
@@ -14,11 +14,18 @@ const flow = Flow.fromJson(example1FlowData)
 
 const outflowSteps = flow.outflow.map(outflow => calcOutflowSteps(outflow, ['#a00', '#0a0']))
 
-export const Example1Svg = () => {
-    const sPos = [25, 150]
-    const vPos = [225, 150]
-    const tPos = [425, 150]
-    const s2Pos = [225 - 200 / Math.sqrt(2), 150 + 200 / Math.sqrt(2)]
+const sPos = [25, 150]
+const vPos = [225, 150]
+const tPos = [425, 150]
+const s2Pos = [225 - 200 / Math.sqrt(2), 150 + 200 / Math.sqrt(2)]
+
+export const Example1Svg = ({ overrideT = null }) => {
+    const width = 450
+    const height = s2Pos[1] + 25
+    const svgIdPrefix = 'example1-'
+    if (typeof overrideT == 'number') {
+        return <SvgContent width={width} height={height} svgIdPrefix={svgIdPrefix} t={overrideT} showStopWatch={false} />
+    }
     return <Stepper values={[0, 400, 800, 1200, 1200.00001, 1450, 1900]} alwaysVisible>
         {
             (value, step, isActive) => {
@@ -30,26 +37,27 @@ export const Example1Svg = () => {
                         return (value || 0)
                     })
                 }, 1 / 30)
-                const width = 450
-                const height = s2Pos[1] + 25
-                const svgIdPrefix = 'example1-'
-                return <div style={{ position: 'relative' }}>
-                    <svg width={width} height={height}>
-                        <SvgDefs svgIdPrefix={svgIdPrefix} />
-                        <FlowEdge svgIdPrefix={svgIdPrefix} outflowSteps={outflowSteps[0]} from={sPos} to={vPos} capacity={capacity[0]} transitTime={transitTime[0]} queue={flow.queues[0]} t={t} />
-                        <FlowEdge svgIdPrefix={svgIdPrefix} outflowSteps={outflowSteps[1]} from={vPos} to={tPos} capacity={capacity[1]} transitTime={transitTime[1]} queue={flow.queues[1]} t={t} />
-                        <FlowEdge svgIdPrefix={svgIdPrefix} visible={t > 1200} outflowSteps={outflowSteps[2]} from={s2Pos} to={vPos} capacity={capacity[2]} transitTime={transitTime[2]} queue={flow.queues[2]} t={t} />
-
-                        <Vertex pos={sPos} label={<TeX>s_1</TeX>} />
-                        <Vertex pos={tPos} label={<TeX>t_1</TeX>} />
-                        <Vertex pos={vPos} label={<TeX>v</TeX>} />
-                        <Vertex pos={s2Pos} label={<TeX>s_2</TeX>} visible={t > 1200} />
-                        <g opacity={typeof value == 'number' ? 1 : 0} style={{transition: 'opacity 0.2s'}}>
-                            <StopWatch t={t} y={10} x={vPos[0] - 20} size={40} />
-                        </g>
-                    </svg>
-                </div>
+                return <SvgContent width={width} height={height} svgIdPrefix={svgIdPrefix} t={t} showStopWatch={typeof value == 'number'} />
             }
         }
     </Stepper>
+}
+
+const SvgContent = ({ width, height, svgIdPrefix, t, showStopWatch}) => {
+    return <div style={{ position: 'relative' }}>
+    <svg width={width} height={height}>
+        <SvgDefs svgIdPrefix={svgIdPrefix} />
+        <FlowEdge svgIdPrefix={svgIdPrefix} outflowSteps={outflowSteps[0]} from={sPos} to={vPos} capacity={capacity[0]} transitTime={transitTime[0]} queue={flow.queues[0]} t={t} />
+        <FlowEdge svgIdPrefix={svgIdPrefix} outflowSteps={outflowSteps[1]} from={vPos} to={tPos} capacity={capacity[1]} transitTime={transitTime[1]} queue={flow.queues[1]} t={t} />
+        <FlowEdge svgIdPrefix={svgIdPrefix} visible={t > 1200} outflowSteps={outflowSteps[2]} from={s2Pos} to={vPos} capacity={capacity[2]} transitTime={transitTime[2]} queue={flow.queues[2]} t={t} />
+
+        <Vertex pos={sPos} label={<TeX>s_1</TeX>} />
+        <Vertex pos={tPos} label={<TeX>t_1</TeX>} />
+        <Vertex pos={vPos} label={<TeX>v</TeX>} />
+        <Vertex pos={s2Pos} label={<TeX>s_2</TeX>} visible={t > 1200} />
+        <g opacity={showStopWatch ? 1 : 0} style={{transition: 'opacity 0.2s'}}>
+            <StopWatch t={t} y={10} x={vPos[0] - 20} size={40} />
+        </g>
+    </svg>
+</div>
 }
