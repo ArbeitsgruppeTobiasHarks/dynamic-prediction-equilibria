2f69991b4c791e8ce6c47e541d06da0e5e3f901c
Michael Markl <marklmichael98@gmail.com>
2022-07-04 18:14:35 -0700

    simplify build_with_times

--- predictor/src/utilities/build_with_times.py
+++ predictor/src/utilities/build_with_times.py
@@ -1,7 +1,6 @@
-import datetime
+from datetime import datetime, timedelta, timezone
+from time import time
 from typing import Optional, Iterable, Tuple
-
-import time
 
 from core.dynamic_flow import DynamicFlow
 from core.flow_builder import FlowBuilder
@@ -12,36 +11,38 @@ def build_with_times(
         observe_commodities: Optional[Iterable[int]] = None, suppress_log: bool = False
 ) -> Tuple[DynamicFlow, float]:
     generator = flow_builder.build_flow()
-    start_time = last_milestone_time = time.time()
+    start_time = last_milestone_time = time()
     flow = next(generator)
     start_date_time = (
-            datetime.datetime(1970, 1, 1) +
-            datetime.timedelta(seconds=round(start_time))
-    ).replace(tzinfo=datetime.timezone.utc).astimezone(tz=None).time()
+            datetime(1970, 1, 1) +
+            timedelta(seconds=round(start_time))
+    ).replace(tzinfo=timezone.utc).astimezone(tz=None).time()
     if not suppress_log:
         print(f"Flow#{flow_id} built until phi={flow.phi}; Started At={start_date_time}")
+        last_log_time = time()
     milestone_interval = reroute_interval
     milestone = milestone_interval
     while flow.phi < horizon:
         flow = next(generator)
         if flow.phi >= milestone:
-            new_milestone_time = time.time()
+            new_milestone_time = time()
             elapsed = new_milestone_time - start_time
             remaining_time = (horizon - flow.phi) * (new_milestone_time - last_milestone_time) / milestone_interval
             finish_time = (
-                    datetime.datetime(1970, 1, 1) +
-                    datetime.timedelta(seconds=round(new_milestone_time + remaining_time))
-            ).replace(tzinfo=datetime.timezone.utc).astimezone(tz=None).time()
-            if not suppress_log:
+                    datetime(1970, 1, 1) +
+                    timedelta(seconds=round(new_milestone_time + remaining_time))
+            ).replace(tzinfo=timezone.utc).astimezone(tz=None).time()
+            if not suppress_log and new_milestone_time - last_log_time >= 1:
                 print(f"Flow#{flow_id} built until phi={flow.phi:.1f};" +
-                      f" Time Elapsed={datetime.timedelta(seconds=round(elapsed))};" +
-                      f" Estimated Remaining Time={datetime.timedelta(seconds=round(remaining_time))};" +
+                      f" Time Elapsed={timedelta(seconds=round(elapsed))};" +
+                      f" Estimated Remaining Time={timedelta(seconds=round(remaining_time))};" +
                       f" Finished at {finish_time};" +
                       (
                           f" TravelTimes={[flow.avg_travel_time(i, flow.phi) for i in observe_commodities]};"
                           if observe_commodities is not None else "")
                       )
+                last_log_time = new_milestone_time
             milestone += milestone_interval
             last_milestone_time = new_milestone_time
-    elapsed = time.time() - start_time
+    elapsed = time() - start_time
     return flow, elapsed
