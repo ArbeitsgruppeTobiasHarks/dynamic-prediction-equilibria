bbd06553181876febfa6c0e26491b405f24fd775
Michael Markl <marklmichael98@gmail.com>
2022-08-11 18:15:55 +0200

    Retrieve costs only when necessary and fix caching active edges

--- predictor/src/core/flow_builder.py
+++ predictor/src/core/flow_builder.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from functools import lru_cache
 
 from typing import Generator, Optional, Dict, List, Set, Tuple
 
@@ -23,9 +24,8 @@ class FlowBuilder:
     _flow: DynamicFlow
     _next_reroute_time: float
     _route_time: float
-    _costs: Dict[PredictorType, List[PiecewiseLinear]]
-    _commodity_ids_by_source: Dict[Node, Set[Commodity]]
-    _commodity_ids_by_sink: Dict[Node, Set[Commodity]]
+    _commodity_ids_by_source: Dict[Node, Set[int]]
+    _commodity_ids_by_sink: Dict[Node, Set[int]]
     _network_inflow_changes: PriorityQueue[Tuple[Commodity, float]]
 
     def __init__(self,
@@ -66,16 +66,33 @@ class FlowBuilder:
         self._network_inflow_changes = PriorityQueue([
             ((c, t), t) for c in network.commodities for t in c.net_inflow.times
         ])
-        self._costs = {}
+
+    @lru_cache(maxsize=None)
+    def _get_costs(self, predictor_type: PredictorType) -> List[PiecewiseLinear]:
+        prediction_time = self._next_reroute_time - self.reroute_interval
+        graph = self.network.graph
+        travel_time = self.network.travel_time
+        capacity = self.network.capacity
+        predictions = self.predictors[predictor_type].predict(
+            prediction_time, self._flow)
+        costs = [
+            PiecewiseLinear(
+                predictions[e].times,
+                [travel_time[e] + value / capacity[e]
+                 for value in predictions[e].values],
+                predictions[e].first_slope / capacity[e],
+                predictions[e].last_slope / capacity[e],
+                (self._flow.phi, float('inf'))
+            )
+            for e in range(len(graph.edges))
+        ]
+        return costs
 
     def build_flow(self) -> Generator[DynamicFlow, None, None]:
         if self._built:
             raise RuntimeError(
                 "Flow was already built. Initialize a new builder.")
         self._built = True
-        graph = self.network.graph
-        travel_time = self.network.travel_time
-        capacity = self.network.capacity
 
         yield self._flow
         while self._flow.phi < float('inf'):
@@ -83,25 +100,7 @@ class FlowBuilder:
                 c, t = self._network_inflow_changes.pop()
                 self._handle_nodes.add(c.source)
             if self.reroute_interval is None or self._flow.phi >= self._next_reroute_time:
-                prediction_time = self._next_reroute_time
-                predictions = {
-                    key: predictor.predict(prediction_time, self._flow)
-                    for (key, predictor) in self.predictors.items()
-                }
-                self._costs = {
-                    key: [
-                        PiecewiseLinear(
-                            prediction[e].times,
-                            [travel_time[e] + value / capacity[e]
-                                for value in prediction[e].values],
-                            prediction[e].first_slope / capacity[e],
-                            prediction[e].last_slope / capacity[e],
-                            (self._flow.phi, float('inf'))
-                        )
-                        for e in range(len(graph.edges))
-                    ]
-                    for (key, prediction) in predictions.items()
-                }
+                self._get_costs.cache_clear()
 
                 self._active_edges = [{} for _ in self.network.commodities]
                 self._route_time = self._next_reroute_time
@@ -129,7 +128,7 @@ class FlowBuilder:
         compute_mode = self.predictors[commodity.predictor_type].compute_mode()
         if compute_mode in [ComputeMode.CONSTANT, ComputeMode.CONSTANT_AND_SAME_FOR_SINK]:
             const_costs = [c.values[0]
-                           for c in self._costs[commodity.predictor_type]]
+                           for c in self._get_costs(commodity.predictor_type)]
             distances = reverse_dijkstra(
                 commodity.sink, const_costs, com_nodes)
             for v in com_nodes:
@@ -147,11 +146,12 @@ class FlowBuilder:
                     self._active_edges[i][v] = active_edges
                 else:
                     for j in self._commodity_ids_by_sink[commodity.sink]:
-                        self._active_edges[j][v] = active_edges
+                        if self.network.commodities[j].predictor_type == commodity.predictor_type:
+                            self._active_edges[j][v] = active_edges
         else:
             # Do Time-Dependent dijkstra from s to t to find active outgoing edges of s
             arrival_times, realised_cost = dynamic_dijkstra(self._route_time, s, sink, com_nodes,
-                                                            self._costs[commodity.predictor_type])
+                                                            self._get_costs(commodity.predictor_type))
 
             # Dijkstra done. Now searching all active edges leading to t.
             active_edges = []
