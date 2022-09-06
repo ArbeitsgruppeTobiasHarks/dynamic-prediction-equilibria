Fix average demand computation

predictor/src/scenarios/sioux_falls_multi_com_scenario.py
@@ -1,6 +1,7 @@
 import json
 import os
 import numpy as np
+from core.dynamic_flow import DynamicFlow
 from core.network import Network
 from core.predictors.constant_predictor import ConstantPredictor
 from core.predictors.linear_predictor import LinearPredictor
@@ -14,7 +15,7 @@ from ml.SKNeighborhood import train_sk_neighborhood_model
 from ml.TFFullNetworkModel import train_tf_full_net_model
 from ml.TFNeighborhood import train_tf_neighborhood_model
 from ml.build_test_flows import build_flows
-from ml.generate_queues import generate_queues_and_edge_loads
+from ml.generate_queues import generate_queues_and_edge_loads, save_queues_and_edge_loads_for_flow
 
 
 def run_scenario(edges_tntp_path: str, nodes_tntp_path: str, od_pairs_file_path: str, scenario_dir: str):
@@ -37,7 +38,7 @@ def run_scenario(edges_tntp_path: str, nodes_tntp_path: str, od_pairs_file_path:
     past_timesteps = 20
     future_timesteps = 20
     prediction_interval = 1.
-    number_training_flows = 500
+    number_training_flows = 11
     number_eval_flows = 20
     max_distance = 3
 
@@ -52,31 +53,45 @@ def run_scenario(edges_tntp_path: str, nodes_tntp_path: str, od_pairs_file_path:
 
     network.print_info()
 
+    def on_flow_computed(flow_id: str, flow: DynamicFlow):
+        out_path = os.path.join(
+            queues_and_edge_loads_dir, f"{flow_id}-queues-and-edge-loads.npy")
+        save_queues_and_edge_loads_for_flow(out_path, past_timesteps, horizon, reroute_interval, prediction_interval,
+                                            flow)
+
     build_flows(network_path, flows_dir, inflow_horizon,
-                number_training_flows, horizon, reroute_interval, demand_sigma, check_for_optimizations=False)
+                number_training_flows, horizon, reroute_interval, demand_sigma, check_for_optimizations=False,
+                on_flow_computed=on_flow_computed)
 
     generate_queues_and_edge_loads(
         past_timesteps, flows_dir, queues_and_edge_loads_dir, horizon, reroute_interval, prediction_interval)
 
     build_tf_full_net_predictor = train_tf_full_net_model(queues_and_edge_loads_dir, past_timesteps, future_timesteps,
-                                                          reroute_interval, prediction_interval, horizon, network, tf_full_net_model_path)
-    build_tf_neighborhood_predictor = train_tf_neighborhood_model(queues_and_edge_loads_dir, past_timesteps, future_timesteps,
-                                                                  reroute_interval, prediction_interval, horizon, network, tf_neighborhood_models_path, max_distance)
+                                                          reroute_interval, prediction_interval, horizon, network,
+                                                          tf_full_net_model_path)
+    build_tf_neighborhood_predictor = train_tf_neighborhood_model(queues_and_edge_loads_dir, past_timesteps,
+                                                                  future_timesteps,
+                                                                  reroute_interval, prediction_interval, horizon,
+                                                                  network, tf_neighborhood_models_path, max_distance)
     build_sk_full_net_predictor = train_sk_full_net_model(queues_and_edge_loads_dir, past_timesteps, future_timesteps,
-                                                          reroute_interval, prediction_interval, horizon, network, sk_full_net_model_path)
-    build_sk_neighborhood_predictor = train_sk_neighborhood_model(queues_and_edge_loads_dir, past_timesteps, future_timesteps,
-                                                                  reroute_interval, prediction_interval, horizon, network, sk_neighborhood_models_path, max_distance)
+                                                          reroute_interval, prediction_interval, horizon, network,
+                                                          sk_full_net_model_path)
+    build_sk_neighborhood_predictor = train_sk_neighborhood_model(queues_and_edge_loads_dir, past_timesteps,
+                                                                  future_timesteps,
+                                                                  reroute_interval, prediction_interval, horizon,
+                                                                  network, sk_neighborhood_models_path, max_distance)
 
-    def build_predictors(network): return {
-        PredictorType.ZERO: ZeroPredictor(network),
-        PredictorType.CONSTANT: ConstantPredictor(network),
-        PredictorType.LINEAR: LinearPredictor(network, pred_horizon),
-        PredictorType.REGULARIZED_LINEAR: RegularizedLinearPredictor(network, pred_horizon, delta=1.),
-        PredictorType.MACHINE_LEARNING_TF_FULL_NET: build_tf_full_net_predictor(network),
-        PredictorType.MACHINE_LEARNING_TF_NEIGHBORHOOD: build_tf_neighborhood_predictor(network),
-        PredictorType.MACHINE_LEARNING_SK_FULL_NET: build_sk_full_net_predictor(network),
-        PredictorType.MACHINE_LEARNING_SK_NEIGHBORHOOD: build_sk_neighborhood_predictor(network),
-    }
+    def build_predictors(network: Network):
+        return {
+            PredictorType.ZERO: ZeroPredictor(network),
+            PredictorType.CONSTANT: ConstantPredictor(network),
+            PredictorType.LINEAR: LinearPredictor(network, pred_horizon),
+            PredictorType.REGULARIZED_LINEAR: RegularizedLinearPredictor(network, pred_horizon, delta=1.),
+            PredictorType.MACHINE_LEARNING_TF_FULL_NET: build_tf_full_net_predictor(network),
+            PredictorType.MACHINE_LEARNING_TF_NEIGHBORHOOD: build_tf_neighborhood_predictor(network),
+            PredictorType.MACHINE_LEARNING_SK_FULL_NET: build_sk_full_net_predictor(network),
+            PredictorType.MACHINE_LEARNING_SK_NEIGHBORHOOD: build_sk_neighborhood_predictor(network),
+        }
 
     # shallow_evaluate_predictors(network_path, flows_dir, shallow_eval_dir, past_timesteps, future_timesteps,
     #                             horizon, reroute_interval, prediction_interval, build_predictors)
@@ -123,8 +138,9 @@ def run_scenario(edges_tntp_path: str, nodes_tntp_path: str, od_pairs_file_path:
     network = Network.from_file(network_path)
     network.print_info()
     print(f"""Average Demand: {np.average([
-        c.net_inflow.values[0]
+        source.values[0]
         for c in network.commodities
+        for source in c.sources.values()
     ])}""")
 
 
@@ -135,5 +151,6 @@ if __name__ == "__main__":
         od_pairs_csv_path = os.path.expanduser("~/git/TransportationNetworks/SiouxFalls/CSV-data/SiouxFalls_od.csv")
         run_scenario(edges_tntp_path, nodes_tntp_path,
                      od_pairs_csv_path, "./out/journal-sioux-falls-multi-com")
+
 
     main()