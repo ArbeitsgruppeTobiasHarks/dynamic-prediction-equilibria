b3bb5da544bbdcd2822c1baf2c0622d50f630ea3
Michael Markl <marklmichael98@gmail.com>
2022-09-21 14:28:55 +0200

    Log at end of build

--- predictor/src/utilities/build_with_times.py
+++ predictor/src/utilities/build_with_times.py
@@ -24,6 +24,28 @@ def build_with_times(
         last_log_time = time()
     milestone_interval = reroute_interval
     milestone = milestone_interval
+    
+    def log_time():
+        nonlocal now
+        nonlocal last_log_time
+        nonlocal start_time
+        nonlocal remaining_time
+        nonlocal new_milestone_time
+        nonlocal finish_time
+        elapsed = now - start_time
+        log_msg = f"\rFlow#{flow_id} built until phi={flow.phi:.1f};"
+        log_msg += f" Elapsed={timedelta(seconds=round(elapsed))};"
+        log_msg += f" Remaining={timedelta(seconds=round(new_milestone_time + remaining_time - now))};"
+        log_msg += f" Finished={finish_time};"
+        if observe_commodities_indices is not None:
+            log_msg += f" TravelTimes={[round(flow.avg_travel_time(i, flow.phi), 4) for i in observe_commodities_indices]};"
+        # Clear rest of the line:
+        log_msg += "\033[K"
+
+        sys.stdout.write(log_msg)
+
+        last_log_time = now
+
     while flow.phi < horizon:
         flow = next(generator)
         if flow.phi >= milestone:
@@ -38,19 +60,11 @@ def build_with_times(
             last_milestone_time = new_milestone_time
         now = time()
         if not suppress_log and now - last_log_time >= 1:
-            elapsed = now - start_time
-            log_msg = f"\rFlow#{flow_id} built until phi={flow.phi:.1f};"
-            log_msg += f" Elapsed={timedelta(seconds=round(elapsed))};"
-            log_msg += f" Remaining={timedelta(seconds=round(new_milestone_time + remaining_time - now))};"
-            log_msg += f" Finished={finish_time};"
-            if observe_commodities_indices is not None:
-                log_msg += f" TravelTimes={[round(flow.avg_travel_time(i, flow.phi), 4) for i in observe_commodities_indices]};"
-            # Clear rest of the line:
-            log_msg += "\033[K"
+            log_time()
+    
+    if not suppress_log:
+        log_time()
 
-            sys.stdout.write(log_msg)
-
-            last_log_time = now
     print()
     elapsed = time() - start_time
     return flow, elapsed
