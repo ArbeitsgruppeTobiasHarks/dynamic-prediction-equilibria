bef8fb31cb6b5b1078a158e2a9e7a504e3be70c6
Michael Markl <marklmichael98@gmail.com>
2022-10-05 17:36:45 +0200

    Remove visualization from this repo Migrated to github.com/ArbeitsgruppeTobiasHarks/dynamic-flow-visualization

--- visualization/src/sample.tsx
+++
@@ -1,9 +0,0 @@
-import { Flow } from "./Flow";
-
-import sampleData from "./sampleFlowData.json"
-import { Network } from './Network';
-
-
-export const network = Network.fromJson(sampleData["network"])
-export const flow = Flow.fromJson(sampleData["flow"])
-
