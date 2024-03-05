d81ce769f51553b597b6a047a828d0bc5d034141
Michael Markl <marklmichael98@gmail.com>
2024-03-05 13:41:15 +0100

    Fix tests

--- src/test/test_bellman_ford.py
+++ src/test/test_bellman_ford.py
@@ -5,34 +5,33 @@ from core.bellman_ford import bellman_ford
 from utilities.piecewise_linear import PiecewiseLinear
 
 
-class TestBellmanFord(unittest.TestCase):
-    def test_flow_bellman_ford(self):
-        network = build_sample_network()
-        costs = [
-            PiecewiseLinear(
-                [2.05, 2.55, 3.55], [1.025, 1.0, 1.0], 0.0, 0.0, (2.05, float("inf"))
-            ),
-            PiecewiseLinear(
-                [2.05, 2.55, 3.55], [3.075, 3.7, 3.7], 0.0, 0.0, (2.05, float("inf"))
-            ),
-            PiecewiseLinear(
-                [2.05, 2.55, 3.55], [1.0, 1.0, 1.0], 0.0, 0.0, (2.05, float("inf"))
-            ),
-            PiecewiseLinear(
-                [2.05, 2.55, 3.55], [1.0, 1.0, 1.0], 0.0, 0.0, (2.05, float("inf"))
-            ),
-            PiecewiseLinear(
-                [2.05, 2.55, 3.55], [1.05, 1.55, 1.55], 0.0, 0.0, (2.05, float("inf"))
-            ),
-        ]
-        phi = 2.075
-        labels = bellman_ford(
-            network.graph.nodes[2], costs, set(network.graph.nodes.values()), 2.075
-        )
-        for v in network.graph.nodes.values():
-            has_active_edge = False
-            for e in v.outgoing_edges:
-                if labels[e.node_to](phi + costs[e.id](phi)) <= labels[v](phi):
-                    has_active_edge = True
-                    break
-            assert has_active_edge or len(v.outgoing_edges) == 0
+def test_flow_bellman_ford():
+    network = build_sample_network()
+    costs = [
+        PiecewiseLinear(
+            [2.05, 2.55, 3.55], [1.025, 1.0, 1.0], 0.0, 0.0, (2.05, float("inf"))
+        ),
+        PiecewiseLinear(
+            [2.05, 2.55, 3.55], [3.075, 3.7, 3.7], 0.0, 0.0, (2.05, float("inf"))
+        ),
+        PiecewiseLinear(
+            [2.05, 2.55, 3.55], [1.0, 1.0, 1.0], 0.0, 0.0, (2.05, float("inf"))
+        ),
+        PiecewiseLinear(
+            [2.05, 2.55, 3.55], [1.0, 1.0, 1.0], 0.0, 0.0, (2.05, float("inf"))
+        ),
+        PiecewiseLinear(
+            [2.05, 2.55, 3.55], [1.05, 1.55, 1.55], 0.0, 0.0, (2.05, float("inf"))
+        ),
+    ]
+    phi = 2.075
+    labels = bellman_ford(
+        network.graph.nodes[2], costs, set(network.graph.nodes.values()), 2.075
+    )
+    for v in network.graph.nodes.values():
+        has_active_edge = False
+        for e in v.outgoing_edges:
+            if labels[e.node_to](phi + costs[e.id](phi)) <= labels[v](phi):
+                has_active_edge = True
+                break
+        assert has_active_edge or len(v.outgoing_edges) == 0
